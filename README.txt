84d13192e92565bcab989ea9d8d44845ff5d7013
Scott Windsor <swindsor@gmail.com>
2013-05-11 16:08:33 -0700

    Initial commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+= FizzBuzz
+FizzBuzz is a classic coding problem given during interviews. While it's fairly trivial, many engineers have issues with this simple question. Here are my solutions in various languages. Use them to learn from, critique, or if you want, evalaute my coding skills. Feel free to use them & learn from them, but please don't re-use them verbatim for professional purposes (come up with your own solution). As a result, I've set these as BSD-clause 2 license.
+
+== ruby
+
+My approach here was to make a flexible "FizzBuzz" class that can use generic rules. I wanted to avoid the class if/then/else code chain, and show how the dynamic nature of ruby can lead to some very readable & flexible code.
+
+== TODO
+
+Other languages? Not sure which ones to do next.
+
---
+++ ruby/fizz_buzz.rb
@@ -0,0 +1,56 @@
+class Bazinator
+  attr_accessor :range
+
+  def initialize(range)
+    @range = range
+  end
+
+  def rules
+    @@rules
+  end
+
+  def default_rule
+    @@default_rule
+  end
+
+  def self.default_rule(rule)
+    @@default_rule = rule
+  end
+
+  def self.rule(rule)
+    @@rules ||= []
+    @@rules << rule
+  end
+
+  def rules
+    @@rules
+  end
+
+  def run_rules(i)
+    results = rules.map{|rule| rule.call(i) }
+    results.delete_if(&:nil?)
+    results << default_rule.call(i) if results.empty?
+    results
+  end
+
+  def each(&block)
+    range.each do |i|
+      yield run_rules(i).join('')
+    end
+  end
+
+  def print
+    each do |item|
+      puts item
+    end
+  end
+end
+
+class FizzBuzz < Bazinator
+  default_rule ->(i){ i }
+  rule ->(i){"Fizz" if i % 3 == 0 }
+  rule ->(i){"Buzz" if i % 5 == 0 }
+end
+
+fb = FizzBuzz.new(1..100)
+fb.print
